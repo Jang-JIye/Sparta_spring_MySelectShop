658c6a83d3543634518c6b453150b16298432520
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-01-22 23:35:25 +0900

    #4 [feat] TimeStamped 구현
    매일 새볔 1시에 관심 상품 목록 제목으로 검색해서 최저가 정보를 업데이트라는 스케쥴러 만들기.(ProductService에도 updateBySearch 생성)

--- src/main/java/com/sparta/myselectshopbeta/entity/Product.java
+++ src/main/java/com/sparta/myselectshopbeta/entity/Product.java
@@ -1,17 +1,19 @@
 package com.sparta.myselectshopbeta.entity;
 
+import com.sparta.myselectshopbeta.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshopbeta.dto.ProductRequestDto;
+import com.sparta.myselectshopbeta.naver.dto.ItemDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import jakarta.persistence.*;
 
 @Getter
 @Setter
 @Entity // DB 테이블 역할을 합니다.
 @NoArgsConstructor
-public class Product {
+public class Product extends Timestamped{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // ID가 자동으로 생성 및 증가합니다.
@@ -38,5 +40,13 @@ public class Product {
         this.link = requestDto.getLink();
         this.lprice = requestDto.getLprice();
         this.myprice = 0;
+    }
+
+    public void update(ProductMypriceRequestDto requestDto) {
+        this.myprice = requestDto.getMyprice();
+    }
+
+    public void updateByItemDto (ItemDto itemDto) {
+        this.lprice = itemDto.getLprice();
     }
 }
